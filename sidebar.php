ea895bfc51d9f514c5aca11cff744eaac3f77709
YanuarRamadhan24 <97354432+YanuarRamadhan24@users.noreply.github.com>
2022-01-08 23:57:57 +0700

    Add files via upload

---
+++ sidebar.php
@@ -0,0 +1,58 @@
+<!-- Sidebar -->
+    <ul class="navbar-nav sidebar sidebar-light accordion" id="accordionSidebar">
+      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
+        <div class="sidebar-brand-icon">
+          <img src="img/logo/2logo.png">
+        </div>
+        <div class="sidebar-brand-text mx-3">Kedai Pandemi</div>
+      </a>
+      <hr class="sidebar-divider my-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>Beranda</span></a>
+      </li>
+      <hr class="sidebar-divider">
+      <div class="sidebar-heading">
+        Data Master
+      </div>
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="PELANGGAN.php" >
+          <i class="far fa-fw fa-user"></i>
+          <span>Master Pelanggan</span>
+        </a>
+      </li>
+	  <li class="nav-item">
+        <a class="nav-link collapsed" href="DAFTAR_MENU.php" >
+          <i class="far fa-fw fa-bell"></i>
+          <span>Daftar Menu</span>
+        </a>
+      </li>
+	  
+	  
+      
+         
+      <hr class="sidebar-divider">
+      <div class="sidebar-heading">
+        Transaksi
+      </div>
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="transaksi.php" >
+          <i class="fas fa-fw fa-columns"></i>
+          <span>Data Transaksi</span>
+        </a>
+        
+      </li>
+	  
+	  <li class="nav-item">
+        <a class="nav-link collapsed" href="laporan.php" >
+          <i class="fas fa-fw fa-file"></i>
+          <span>Laporan</span>
+        </a>
+        
+      </li>
+     
+      <hr class="sidebar-divider">
+      <div class="version" id="version-ruangadmin"></div>
+    </ul>
+    <!-- Sidebar -->
